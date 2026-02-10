dc1f42e8cc8c46536c8558bdda72ab6b7ee3d3b2
Michael Burbidge <mburbidg@gmail.com>
2026-02-10 09:36:48 -0500

    Extend with some simple REST endpoints.

--- src/fylio/server.clj
+++ src/fylio/server.clj
@@ -1,12 +1,48 @@
 (ns fylio.server
   (:gen-class)
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require
+    [cheshire.core :as json]
+    [reitit.ring :as ring]
+    [ring.adapter.jetty :as jetty]
+    [ring.util.response :as resp]))
 
-(defn handler [_request]
-  {:status  200
-   :headers {"content-type" "text/plain; charset=utf-8"}
-   :body    "Hello, World!\n"})
+(defn json-response
+  ([data] (json-response data 200))
+  ([data status]
+   (-> (resp/response (json/generate-string data))
+       (resp/status status)
+       (resp/header "content-type" "application/json; charset=utf-8"))))
+
+(defn read-json-body [req]
+  ;; Reads JSON body into a Clojure map (or nil if no body)
+  (when-let [body (:body req)]
+    (let [s (slurp body)]
+      (when (seq s)
+        (json/parse-string s true)))))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/health"
+        {:get (fn [_] (resp/response "OK\n"))}]
+
+       ["/api"
+        ["/hello"
+         {:get (fn [_] (json-response {:message "Hello, World!"}))}]
+
+        ["/hello/:name"
+         {:get (fn [req]
+                 (let [name (get-in req [:path-params :name])]
+                   (json-response {:message (str "Hello, " name "!")})))}]
+
+        ["/echo"
+         {:post (fn [req]
+                  (let [body (read-json-body req)]
+                    (if body
+                      (json-response {:you_sent body})
+                      (json-response {:error "Expected JSON body"} 400))))}]]])
+    (ring/create-default-handler)))
 
 (defn -main [& _args]
-  (println "Starting server on http://localhost:3000")
-  (jetty/run-jetty handler {:port 3000 :join? true}))
+  (println "REST server on http://localhost:3000")
+  (jetty/run-jetty app {:port 3000 :join? true}))
